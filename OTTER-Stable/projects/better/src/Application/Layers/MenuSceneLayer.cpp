30209c5990ada46097151fbfc185a7a1411426a0
Abdalla Mohamed <abdulla.mohamed.university@gmail.com>
2022-04-05 16:48:17 -0400

    Menu Scene template

---
+++ OTTER-Stable/projects/better/src/Application/Layers/MenuSceneLayer.cpp
@@ -0,0 +1,186 @@
+#include "MenuSceneLayer.h"
+// GLM math library
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <GLM/gtx/common.hpp> // for fmod (floating modulus)
+#include <fstream>
+#include <filesystem>
+#include "Application/Timing.h"
+
+// Graphics
+#include "Graphics/Buffers/IndexBuffer.h"
+#include "Graphics/Buffers/VertexBuffer.h"
+#include "Graphics/VertexArrayObject.h"
+#include "Graphics/ShaderProgram.h"
+#include "Graphics/Textures/Texture2D.h"
+#include "Graphics/Textures/TextureCube.h"
+#include "Graphics/VertexTypes.h"
+#include "Graphics/Font.h"
+#include "Graphics/GuiBatcher.h"
+#include "Graphics/Framebuffer.h"
+#include "Graphics/Framebuffer.h"
+
+// Utilities
+#include "Utils/MeshBuilder.h"
+#include "Utils/MeshFactory.h"
+#include "Utils/ObjLoader.h"
+#include "Utils/ImGuiHelper.h"
+#include "Utils/ResourceManager/ResourceManager.h"
+#include "Utils/FileHelpers.h"
+#include "Utils/JsonGlmHelpers.h"
+#include "Utils/StringUtils.h"
+#include "Utils/GlmDefines.h"
+#include "ToneFire.h"
+
+// Gameplay
+#include "Gameplay/Material.h"
+#include "Gameplay/GameObject.h"
+#include "Gameplay/Scene.h"
+
+// Components
+#include "Gameplay/Components/IComponent.h"
+#include "Gameplay/Components/Camera.h"
+#include "Gameplay/Components/RotatingBehaviour.h"
+#include "Gameplay/Components/RenderComponent.h"
+#include "Gameplay/Components/MaterialSwapBehaviour.h"
+#include "Gameplay/Components/TriggerVolumeEnterBehaviour.h"
+#include "Gameplay/Components/SimpleCameraControl.h"
+#include "Gameplay/Components/Light.h"
+
+//Rubbish Rush Components
+#include "Gameplay/Components/AudioEngine.h"
+#include "Gameplay/Components/ShadowCamera.h"
+
+// Physics
+#include "Gameplay/Physics/RigidBody.h"
+#include "Gameplay/Physics/Colliders/BoxCollider.h"
+#include "Gameplay/Physics/Colliders/PlaneCollider.h"
+#include "Gameplay/Physics/Colliders/SphereCollider.h"
+#include "Gameplay/Physics/Colliders/ConvexMeshCollider.h"
+#include "Gameplay/Physics/Colliders/CylinderCollider.h"
+#include "Gameplay/Physics/TriggerVolume.h"
+#include "Graphics/DebugDraw.h"
+
+// GUI
+#include "Gameplay/Components/GUI/RectTransform.h"
+#include "Gameplay/Components/GUI/GuiPanel.h"
+#include "Gameplay/Components/GUI/GuiText.h"
+#include "Gameplay/InputEngine.h"
+
+#include "Application/Application.h"
+#include "Gameplay/Components/ParticleSystem.h"
+#include "Gameplay/InputEngine.h"
+#include "Application/Layers/RenderLayer.h"
+
+MenuSceneLayer::MenuSceneLayer()
+{
+	Name = "Menu Scene";
+	Overrides = AppLayerFunctions::OnAppLoad | AppLayerFunctions::OnUpdate;
+}
+
+MenuSceneLayer::~MenuSceneLayer() = default;
+
+void MenuSceneLayer::OnAppLoad(const nlohmann::json& config)
+{
+	_CreateScene();
+}
+
+void MenuSceneLayer::OnUpdate()
+{
+
+}
+
+void MenuSceneLayer::_CreateScene()
+{
+
+	Application& app = Application::Get();
+	//all conditions to change between in-game conditions and menus
+	bool loadScene = false;
+
+	// For now we can use a toggle to generate our scene vs load from file
+	if (loadScene && std::filesystem::exists("scene.json")) {
+		app.LoadScene("scene.json");
+	}
+	else {
+
+		// This shader handles our basic materials without reflections (cause they expensive)
+		ShaderProgram::Sptr rackShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/basic.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/deferred_forwardRACK.glsl" }
+		});
+
+		// ANIMATION SHADER??
+		ShaderProgram::Sptr animShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/morph.vert" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/deferred_forward.glsl" }
+		});
+
+
+		// This shader handles our foliage vertex shader example
+		ShaderProgram::Sptr foliageShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/foliage.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/deferred_forward.glsl" }
+		});
+
+		//for moving the conveyor belt?
+		ShaderProgram::Sptr conveyorShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/basic_moving.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/deferred_forwardCONVEYOR.glsl" }
+		});
+
+		///////////////////// NEW SHADERS ////////////////////////////////////////////
+
+			// This shader handles our displacement mapping example
+		ShaderProgram::Sptr displacementShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/displacement_mapping.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/deferred_forward.glsl" }
+		});
+		displacementShader->SetDebugName("Displacement Mapping");
+
+		// This shader handles our multitexturing example
+		ShaderProgram::Sptr multiTextureShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/vert_multitextured.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/frag_multitextured.glsl" }
+		});
+		// This shader handles our cel shading example
+		ShaderProgram::Sptr celShader = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/displacement_mapping.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/cel_shader.glsl" }
+		});
+		celShader->SetDebugName("Cel Shader");
+
+		// Basic gbuffer generation with no vertex manipulation
+		ShaderProgram::Sptr deferredForward = ResourceManager::CreateAsset<ShaderProgram>(std::unordered_map<ShaderPartType, std::string>{
+			{ ShaderPartType::Vertex, "shaders/vertex_shaders/basic.glsl" },
+			{ ShaderPartType::Fragment, "shaders/fragment_shaders/deferred_forward.glsl" }
+		});
+		deferredForward->SetDebugName("Deferred - GBuffer Generation");
+
+#pragma region Basic Texture Creation
+		Texture2DDescription singlePixelDescriptor;
+		singlePixelDescriptor.Width = singlePixelDescriptor.Height = 1;
+		singlePixelDescriptor.Format = InternalFormat::RGB8;
+
+		float normalMapDefaultData[3] = { 0.5f, 0.5f, 1.0f };
+		Texture2D::Sptr normalMapDefault = ResourceManager::CreateAsset<Texture2D>(singlePixelDescriptor);
+		normalMapDefault->LoadData(1, 1, PixelFormat::RGB, PixelType::Float, normalMapDefaultData);
+
+		float solidBlack[3] = { 0.5f, 0.5f, 0.5f };
+		Texture2D::Sptr solidBlackTex = ResourceManager::CreateAsset<Texture2D>(singlePixelDescriptor);
+		solidBlackTex->LoadData(1, 1, PixelFormat::RGB, PixelType::Float, solidBlack);
+
+		float solidGrey[3] = { 0.0f, 0.0f, 0.0f };
+		Texture2D::Sptr solidGreyTex = ResourceManager::CreateAsset<Texture2D>(singlePixelDescriptor);
+		solidGreyTex->LoadData(1, 1, PixelFormat::RGB, PixelType::Float, solidGrey);
+
+		float solidWhite[3] = { 1.0f, 1.0f, 1.0f };
+		Texture2D::Sptr solidWhiteTex = ResourceManager::CreateAsset<Texture2D>(singlePixelDescriptor);
+		solidWhiteTex->LoadData(1, 1, PixelFormat::RGB, PixelType::Float, solidWhite);
+
+#pragma endregion 
+
+		//LOAD OBJECTS
+
+}
