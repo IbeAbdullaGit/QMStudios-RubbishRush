d16b4f7f3b9cf460b984ecee96c37705b0122625
KeikakuDorito <nathan.woo77@gmail.com>
2022-04-08 20:49:44 -0400

    Recycle Error sound fixed

--- OTTER-Stable/projects/better/src/Gameplay/Components/CollectTrashBehaviour.cpp
+++ OTTER-Stable/projects/better/src/Gameplay/Components/CollectTrashBehaviour.cpp
@@ -81,6 +81,9 @@ void CollectTrashBehaviour::Update(float deltaTime)
 					_scene->held_recycle += 1;
 					AudioEngine::playEventS("event:/Sounds/SoundEffects/Pickups interactions/PickUpCup");
 				}
+				else if (_scene->held_recycle > 1) {
+					AudioEngine::playEventS("event:/Sounds/SoundEffects/Pickups interactions/TrashPickupStopped");
+				}
 				else //we are recycle but recycle inv is full
 				{
 					std::cout << "We're already holding a recycle item\n";
